3b8e3533893101acd78821869367a45bd9f83757
Ivan Sheyenko <ivs@sitecore.net>
2018-05-08 18:08:39 -0400

    wrap exposrted values in single quotes.

---
+++ src/Sitecore.Support.220469/GetContactRows.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Web;
+using Sitecore.Diagnostics;
+using Sitecore.ListManagement.ContentSearch.Model;
+
+namespace Sitecore.Support.ListManagement.ContentSearch.Pipelines.ExportContacts
+{
+  public class GetContactRows: Sitecore.ListManagement.ContentSearch.Pipelines.ExportContacts.GetContactRows
+  {
+    protected override IEnumerable<string> GetRow(IEnumerable<ContactData> contacts)
+    {
+      Assert.ArgumentNotNull(contacts, "contacts");
+      string sep = this.Delimiter.ToString(CultureInfo.InvariantCulture);
+      yield return string.Join(sep, "Identifier", "FirstName", "Surname", "Email");
+      foreach (ContactData contact in contacts)
+      {
+        yield return string.Join(sep, "'"+contact.Identifier+"'", "'" + contact.FirstName + "'", "'" + contact.Surname + "'", "'" + contact.PreferredEmail + "'");
+      }
+    }
+  }
+}
---
+++ src/Sitecore.Support.220469/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Sitecore.Support.220469")]
+[assembly: AssemblyProduct("Sitecore.Support.220469")]
+[assembly: ComVisible(false)]
